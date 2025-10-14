3650d5376a4758bd6531dd21a0f6e99c0bbda416
George Song <george@gsong.dev>
2025-10-13 18:05:43 -0700

    refactor: remove macOS man page override from bashrc
    Remove Apple Terminal man page viewer override as it interferes
    with terminal workflows and tmux usage.

--- .bashrc.d/20-functions.bash
+++ .bashrc.d/20-functions.bash
@@ -1,10 +1,3 @@
-## Use Apple's man page viewer if we are on a local console
-if [[ "$TERM_PROGRAM" == "Apple_Terminal" ]] || [[ "$TERM_PROGRAM" == "tmux" ]]; then
-	function man {
-		open x-man-page://"$1"
-	}
-fi
-
 ## tab completion for bash autocomplete
 if ! command -v _split_longopt &>/dev/null; then
 	_split_longopt() {
