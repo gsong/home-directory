bf200811e0c73564be3e9011a6d895435987cae4
George Song <george@gsong.dev>
2025-10-26 15:39:55 -0700

    refactor: clean up and improve Neovim configuration
    - Remove unused custom macro autocmd setup
    - Add nil safety check for VIMRUNTIME environment variable
    - Clean up commented colorscheme configurations
    - Simplify LSP keymap configuration using servers.* pattern
    - Fix treesitter plugin config (run -> build)
    - Remove obsolete coc.nvim comment from ufo.lua

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -4,13 +4,12 @@ return {
     opts = {
       servers = {
         prismals = {},
+        ["*"] = {
+          keys = {
+            { "<leader>cl", "<cmd>LspRestart<cr>", desc = "Lsp Restart" },
+          },
+        },
       },
     },
-
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- change a keymap
-      keys[#keys + 1] = { "<leader>cl", "<cmd>LspRestart<cr>", desc = "Lsp Restart" }
-    end,
   },
 }
